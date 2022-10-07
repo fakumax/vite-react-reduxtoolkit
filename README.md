31df7f8132c48996710b94df541892c916ad702e
Facundo Vergara <fakumax@gmail.com>
2022-10-07 20:46:43 -0300

    Add Persist storage

---
+++ README.md
@@ -0,0 +1,9 @@
+#VITE JS, REACT with Redux Toolkit
+
+this project is persistent and contain:
+
+*vite js
+*react
+*redux toolkit
+*redux persist
+\*redux thunk
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+/* async storage */
+//import AsyncStorage from '@react-native-community/async-storage';
+
 import usersReducer from '../redux/users/usersSlice';
 import eveReducer from '../redux/eve/eveSlice';
 
-const store = configureStore({
-  reducer: {
-    users: usersReducer,
-    eve: eveReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducers = combineReducers({
+  users: usersReducer,
+  eve: eveReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
